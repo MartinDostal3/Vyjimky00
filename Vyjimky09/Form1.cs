704d121a6e942d09b68d5a0c260a3342ef1e314c
dostalm.04 <dostalm.04@spst.eu>
2023-01-12 11:31:01 +0100

    ags

---
+++ Vyjimky09/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vyjimky09
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(textBox1.Text);
+            FileStream fs = new FileStream(@"..\..\cisla.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryReader br = new BinaryReader(fs);
+            while(br.BaseStream.Position < br.BaseStream.Length)
+            {
+                int x = br.ReadByte();
+
+            }
+
+
+
+        }
+    }
+}
